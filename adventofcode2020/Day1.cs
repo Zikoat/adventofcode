db291b3ee1a6606e82723806b82462ceb28483a4
Sigurd Schoeler <sschoeler99@gmail.com>
2020-12-08 15:15:33 +0100

    Create Rider project

---
+++ adventofcode2020/Day1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace adventofcode2020
+{
+    public class Day1
+    {
+        public static int Part1(string? input)
+        {
+            input = input.GetValueOrDefault(File.ReadAllText("./day1.txt"))
+            
+            Console.WriteLine(input);
+        }
+    }
+}
