9c1e875f82216ef63cc5aad1754c9341c891ffc4
Sigurd Schoeler <sschoeler99@gmail.com>
2020-12-25 23:21:16 +0100

    Day 1: pass test1 and test2. Correct answer.

--- Tests/Day1Test.cs
+++ Tests/Day1Test.cs
@@ -1,14 +1,50 @@
-﻿using NUnit.Framework;
+﻿using System;
+using adventofcode2020;
+using NUnit.Framework;
 
 namespace Tests
 {
     [TestFixture]
     public class Day1Test
     {
-        [Test]
-        public void Test1()
+        [SetUp]
+        public void Setup()
         {
-            Assert.AreEqual(3, adventofcode2020.Program.SumNumbers(1, 2));
         }
+
+        [Test]
+        public void Test()
+        {
+            string testinput = @"1721
+            979
+            366
+            299
+            675
+            1456";
+            Assert.AreEqual(514579, Day1.Part1(testinput));
+            Console.WriteLine(Day1.Part1(null));
+            
+            Assert.AreEqual(241861950, Day1.Part2(testinput));
+            Console.WriteLine(Day1.Part2(null));
+        }
+        [Test]
+        public void Test2()
+        {
+            string testinput = @"1-3 a: abcde
+                1-3 b: cdefg
+                2-9 c: ccccccccc";
+            
+            Assert.AreEqual(514579, Day2.Part1(testinput));
+            Console.WriteLine(Day2.Part1(null));
+            
+            Assert.AreEqual(241861950, Day2.Part2(testinput));
+            Console.WriteLine(Day2.Part2(null));
+        }
+
+    }
+
+    [TestFixture]
+    public class Day2Test
+    {
     }
 }
--- adventofcode2020/Day1.cs
+++ adventofcode2020/Day1.cs
@@ -1,14 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace adventofcode2020
 {
-    public class Day1
+    public static class Day1
     {
-        public static void Part1(string? input)
+        public static int Part1(string? input)
         {
-            input ??= File.ReadAllText("./day1.txt");
-            Console.WriteLine(input);
+            input ??= File.ReadAllText("../../../..//adventofcode2020/day1.txt");
+            IEnumerable<int> lines = input.Split("\n").Select(val=>Int32.Parse(val.Trim()));
+
+            var output = 0;
+            foreach (var line1 in lines)
+            {
+                foreach (var line2 in lines)
+                {
+                    if (line1 + line2 == 2020)
+                    {
+                        output = line1 * line2;
+                    }
+                }
+
+            }
+            
+            return output;
+        }
+
+        public static int Part2(string? input)
+        {
+            input ??= File.ReadAllText("../../../..//adventofcode2020/day1.txt");
+            IEnumerable<int> lines = input.Split("\n").Select(val=>Int32.Parse(val.Trim()));
+
+            var output = 0;
+            foreach (var line1 in lines)
+            {
+                foreach (var line2 in lines)
+                {
+                    foreach (var line3 in lines)
+                    {
+                        if (line1 + line2 + line3 == 2020)
+                        {
+                            output = line1 * line2 * line3;
+                        }
+                    }
+                }
+            }
+            
+            return output;
+
         }
     }
 }
