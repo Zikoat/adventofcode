6ad1927cda9fa0dc746857acc4ca11ab4628af3f
Sigurd Schoeler <sschoeler99@gmail.com>
2020-12-28 06:13:03 +0100

    Day 3: Start

---
+++ Tests/Day3Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using adventofcode2020;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Day3Test
+    {
+        private const string TestInput = @"..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#";
+
+        [Test]
+        public void Part1()
+        {
+            Assert.AreEqual(7, new Day3(TestInput).Part1());
+            Console.WriteLine(new Day3(TestInput).Part1());
+        }
+    }
+}
---
+++ adventofcode2020/Day3.cs
@@ -0,0 +1,19 @@
+﻿namespace adventofcode2020
+{
+    public class Day3 : Day
+    {
+        public Day3(string? input) : base(input)
+        {
+        }
+
+        public override int Part1()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int Part2()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
