29b6bb79548d3f28f75d2e3bac1e7ab6129f5fd6
Sigurd Schoeler <sschoeler99@gmail.com>
2020-12-28 03:33:24 +0100

    Day 2: part 1 and part 2

--- Tests/Day1Test.cs
+++ Tests/Day1Test.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using adventofcode2020;
 using NUnit.Framework;
 
@@ -27,24 +28,82 @@ namespace Tests
             Assert.AreEqual(241861950, Day1.Part2(testinput));
             Console.WriteLine(Day1.Part2(null));
         }
-        [Test]
-        public void Test2()
-        {
-            string testinput = @"1-3 a: abcde
-                1-3 b: cdefg
-                2-9 c: ccccccccc";
-            
-            Assert.AreEqual(514579, Day2.Part1(testinput));
-            Console.WriteLine(Day2.Part1(null));
-            
-            Assert.AreEqual(241861950, Day2.Part2(testinput));
-            Console.WriteLine(Day2.Part2(null));
-        }
-
     }
 
     [TestFixture]
-    public class Day2Test
+    public class Day2Test// todo move to own file
+
     {
+    const string TestInput = @"1-3 a: abcde
+                1-3 b: cdefg
+                2-9 c: ccccccccc";
+
+    [Test]
+    public void TestPart1()
+    {
+        Assert.AreEqual(2, Day2.Part1(TestInput));
+        int answer = Day2.Part1(null);
+        Assert.Greater(answer, 231);
+        Console.WriteLine(answer);
+    }
+
+    [Test]
+    public void TestStringToTuple()
+    {
+        var generatedOutput = Day2.StringToPasswordTuples("1-3 a: abcde").First();
+        (int, int, char, string) expected = (1, 3, 'a', "abcde");
+        Assert.AreEqual(expected, generatedOutput);
+
+    }
+
+    [Test]
+    public void testStringToTupleLeadingAndTrailingSpaces()
+    {
+        var generatedOutput = Day2.StringToPasswordTuples(@"  1-3 b: cdefg").First();
+        Assert.AreEqual((1, 3, 'b', "cdefg"), generatedOutput);
+    }
+
+    [Test]
+    public void TestStringToTupleMultipleLines()
+    {
+        var passwords = Day2.StringToPasswordTuples(TestInput);
+        Assert.AreEqual(3, passwords.Count());
+    }
+
+    [Test]
+    public void TestIsValidTrue()
+    {
+        Assert.IsTrue(Day2.IsValidBetween((1, 3, 'a', "abcde")));
+
+    }
+
+    [Test]
+    public void IsValidFalse()
+    {
+        Assert.IsFalse(Day2.IsValidBetween((1, 3, 'b', "cdefg")));
+    }
+
+    [Test]
+    public void BoundsAbove10()
+    {
+        var generatedOutput = Day2.StringToPasswordTuples("1-10 a: aabcde").First();
+        Assert.AreEqual((1, 10, 'a', "aabcde"), generatedOutput);
+    }
+
+    [Test]
+    public void IsValidAtPosition()
+    {
+        var passwordTuples = Day2.StringToPasswordTuples(TestInput);
+        Assert.AreEqual(new[] {true, false, false}, passwordTuples.Select(Day2.IsValidAtPosition));
+        
+        Console.WriteLine(Day2.Part2(null));
+    }
+
+    [Test]
+    public void IsValidAtPositionOutOfBounds()
+    {
+        var passwordTuple = Day2.StringToPasswordTuples("2-9 c: ccccccccc").First();
+        Assert.IsFalse(Day2.IsValidAtPosition(passwordTuple));
+    }
     }
 }
--- adventofcode2020/Day2.cs
+++ adventofcode2020/Day2.cs
@@ -2,31 +2,73 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
-namespace Tests
+namespace adventofcode2020
 {
     public static class Day2
     {
+        private const string RegexPattern = @"(\d+)-(\d+) ([a-z]): ([a-z]*)";
+
         public static int Part1(string? input)
         {
             input ??= File.ReadAllText("../../../..//adventofcode2020/day2.txt");
-            IEnumerable<int> lines = input.Split("\n").Select(val=>Int32.Parse(val.Trim()));
-
-            var output = 0;
-            foreach (var line1 in lines)
-            {
-                foreach (var line2 in lines)
-                {
-                    if (line1 + line2 == 2020)
-                    {
-                        output = line1 * line2;
-                    }
-                }
-
-            }
             
+            var passwords = StringToPasswordTuples(input);
+
+            var output = passwords.Count(IsValidBetween);
+
             return output;
         }
 
+        public static int Part2(string? input)
+        {
+            input ??= File.ReadAllText("../../../..//adventofcode2020/day2.txt");
+            
+            var passwords = StringToPasswordTuples(input);
+            var output = passwords.Count(IsValidAtPosition);
+
+            return output;
+
+        }
+
+        public static IEnumerable<(int, int, char, string)> StringToPasswordTuples(string input)
+        {
+            List<(int, int, char, string)> passwordTuples = new List<(int, int, char, string)>();
+            
+            var matches = Regex.Matches(input, RegexPattern);
+            
+            foreach (Match m in matches)
+            {
+                var passwordTuple = MatchToPasswordTuple(m);
+                passwordTuples.Add(passwordTuple);
+            }
+
+            return passwordTuples;
+        }
+
+        private static (int min, int max, char character, string password) MatchToPasswordTuple(Match m)
+        {
+            int min = Int32.Parse(m.Groups[1].Value);
+            int max = Int32.Parse(m.Groups[2].Value);
+            char character = char.Parse(m.Groups[3].Value);
+            string password = m.Groups[4].Value;
+
+            return (min, max, character, password);
+        }
+        
+        public static bool IsValidBetween((int min, int max, char character, string password) passwordTuple)
+        {
+            int characterCount = passwordTuple.password.Count(character => character == passwordTuple.character);
+            return characterCount >= passwordTuple.min && characterCount <= passwordTuple.max;
+        }
+        
+        public static bool IsValidAtPosition((int min, int max, char character, string password) passwordTuple)
+        {
+            var (min, max, character, password) = passwordTuple;
+            
+            return password[min-1] == character ^ password[max-1] == character;
+            
+        }
     }
 }
