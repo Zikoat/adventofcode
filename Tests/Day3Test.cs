a01ef0950434bccc23521d9eb00349f8bf8a2ff5
Sigurd Schoeler <sschoeler99@gmail.com>
2020-12-30 08:43:49 +0100

    Day 3

--- Tests/Day3Test.cs
+++ Tests/Day3Test.cs
@@ -23,7 +23,14 @@ namespace Tests
         public void Part1()
         {
             Assert.AreEqual(7, new Day3(TestInput).Part1());
-            Console.WriteLine(new Day3(TestInput).Part1());
+            Assert.AreEqual(247, new Day3(null).Part1());
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Assert.AreEqual(336, new Day3(TestInput).Part2());
+            Assert.AreEqual(2983070376, new Day3(null).Part2());
         }
     }
 }
--- adventofcode2020/Day3.cs
+++ adventofcode2020/Day3.cs
@@ -1,19 +1,65 @@
-﻿namespace adventofcode2020
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace adventofcode2020
 {
     public class Day3 : Day
     {
+        private IEnumerable<bool>[] map;
         public Day3(string? input) : base(input)
         {
+            map = InputToMap();
         }
 
-        public override int Part1()
+        public override long Part1()
         {
-            throw new System.NotImplementedException();
+            var total = TraverseMapWithSlope(3, 1);
+            return total;
         }
 
-        public override int Part2()
+        private int TraverseMapWithSlope(int skipx, int skipy)
         {
-            throw new System.NotImplementedException();
+            var xpos = 0;
+            var height = map.Length;
+            var width = map.First().Count();
+            var total = 0;
+            for (var i = 0; i < height; i+=skipy)
+            {
+                var line = map.ElementAt(i);
+                total += Convert.ToInt32(line.ElementAt(xpos));
+                xpos = (xpos + skipx) % width;
+            }
+
+            return total;
+        }
+
+        private IEnumerable<bool>[] InputToMap()
+        {
+            return Input
+                .Split(new[] {"\r\n", "\r", "\n"}, StringSplitOptions.None)
+                .Select(s => s.Select(c => c == '#'))
+                .ToArray();
+        }
+
+        public override long Part2()
+        {
+            var slopes = new[]
+            {
+                ( 1, 1 ),
+                ( 3, 1 ), 
+                ( 5, 1 ),
+                ( 7, 1 ),
+                ( 1, 2 )
+            };
+
+            return AggregateSlopes(slopes, (x, y) => x*y);
+        }
+
+        private long AggregateSlopes((int, int)[] slopes, Func<long, long, long> aggregateFunction)
+        {
+            return slopes.Aggregate<(int, int), long>(1, (current, slope) => 
+                aggregateFunction(current, TraverseMapWithSlope(slope.Item1, slope.Item2)));
         }
     }
 }
