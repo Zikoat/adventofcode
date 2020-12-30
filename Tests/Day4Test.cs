dc2882937d457ce564ed5bf8b3d5fa8d211809d3
Sigurd Schoeler <sschoeler99@gmail.com>
2020-12-30 09:39:56 +0100

    Day 4: part1 finished, debugging part2

---
+++ Tests/Day4Test.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using adventofcode2020;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Day4Test
+    {
+        private const string TestInput = @"ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in";
+
+        private const string TestInput2Invalid = @"eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007";
+        private const string TestInput2Valid = @"eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007";
+
+        [Test]
+        public void Part1()
+        {
+            Assert.AreEqual(2, new Day4(TestInput).Part1());
+            // Console.WriteLine(new Day4(null).Part1());
+            Assert.AreEqual(264, new Day4(null).Part1());
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Assert.AreEqual(4, new Day4(TestInput2Valid).Part2());
+            Assert.AreEqual(4, new Day4(TestInput2Invalid).Part2());
+            Console.WriteLine(new Day4(null).Part1());
+            Assert.AreEqual(2983070376, new Day4(null).Part2());
+        }
+    }
+}
---
+++ adventofcode2020/Day4.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace adventofcode2020
+{
+    public class Day4 : Day
+    {
+        public Day4(string? input) : base(input)
+        {
+        }
+
+        public override long Part1()
+        {
+            var passports = Input.Split(Environment.NewLine + Environment.NewLine).Select(passportString =>
+                Regex.Split(passportString, Environment.NewLine + "| ", RegexOptions.IgnoreCase).ToDictionary(keyvalue=> keyvalue.Split(":")[0],keyvalue=>keyvalue.Split(":")[1]));
+
+            // PrintPassports(passports);
+
+            var totalValid = 0;
+            foreach (var passport in passports)
+            {
+                if (
+                    passport.ContainsKey("byr") 
+                    && passport.ContainsKey("iyr") 
+                    && passport.ContainsKey("eyr") 
+                    && passport.ContainsKey("hgt") 
+                    && passport.ContainsKey("hcl") 
+                    && passport.ContainsKey("ecl") 
+                    && passport.ContainsKey("pid") 
+                    // && passport.ContainsKey("cid")
+                    && Int64.Parse(passport["byr"])>=1920
+                    && Int64.Parse(passport["byr"])<=2002
+                    && Int64.Parse(passport["iyr"])>=2010
+                    && Int64.Parse(passport["iyr"])<=2020
+                    && Int64.Parse(passport["eyr"])>=2020
+                    && Int64.Parse(passport["eyr"])<=2030
+                    && ValidateHeight(passport["hgt"])
+                    && Regex.IsMatch(passport["hcl"],@"#[0-9a-f]{6}")
+                    && Regex.IsMatch(passport["ecl"], @"amb|blu|brn|gry|grn|hzl|oth")
+                    && Regex.IsMatch(passport["pid"],@"\d{9}")
+                )
+                {
+                    totalValid++;
+                }
+                     
+            }
+            return totalValid;
+        }
+
+        private static bool ValidateHeight(string height)
+        {
+            var splitHeight = Regex.Split(height, @"(\d+)(\D+)");
+            var amount = Int64.Parse(splitHeight[1]);
+            var unit = splitHeight[2];
+            
+            if (unit == "cm")
+                return amount >= 150 && amount <= 193;
+            if (unit == "in")
+                return amount >= 59 && amount <= 76;
+            
+            throw new Exception("height did not contain cm or in");
+        }
+
+        private static void PrintPassports(IEnumerable<Dictionary<string, string>> passports)
+        {
+            foreach (var passport in passports)
+            {
+                PrintDict(passport);
+                Console.WriteLine();
+            }
+        }
+
+        public override long Part2()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public static void PrintDict(Dictionary<string, string> dict)
+        {
+            foreach (KeyValuePair<string, string> kvp in dict)
+            {
+                //textBox3.Text += ("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+                Console.WriteLine("{0}:{1}", kvp.Key, kvp.Value);
+            }
+
+        }
+    }
+}
