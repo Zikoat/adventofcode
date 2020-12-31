63fc0c04e032dac22799e31373ac601961ffe1ae
Sigurd Schoeler <sschoeler99@gmail.com>
2020-12-31 16:20:26 +0100

    Create test template

---
+++ Tests/DayTestTemplate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using adventofcode2020;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class DayTest
+    {
+        private const string TestInput = @"";
+
+        [Test]
+        public void Part1()
+        {
+            // Assert.AreEqual(-1, new Day(TestInput).Part1());
+            // Assert.AreEqual(-1, new Day(null).Part1());
+        }
+
+        [Test]
+        public void Part2()
+        {
+            // Assert.AreEqual(-1, new Day(TestInput).Part2());
+            // Assert.AreEqual(-1, new Day(null).Part2());
+        }
+    }
+}
