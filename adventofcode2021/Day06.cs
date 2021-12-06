7f7df87e100f6d28708bed6962db5503539d8c2f
Sigurd Schoeler <sschoeler99@gmail.com>
2021-12-06 17:25:09 +0100

    Reformatting

--- adventofcode2021/Day06.cs
+++ adventofcode2021/Day06.cs
@@ -1,6 +1,4 @@
-﻿
-using NUnit.Framework;
-using static System.Environment;
+﻿using NUnit.Framework;
 
 namespace adventofcode2021;
 
@@ -12,10 +10,10 @@ public class Day06 : DayBase
     public override void TestPart1()
     {
         var fishesCount = SimulateFishes(TestInput, 18);
-        
+
         Assert.That(fishesCount, Is.EqualTo(26));
     }
-    
+
     [Test]
     public void TestPart180Steps()
     {
@@ -26,7 +24,7 @@ public class Day06 : DayBase
 
     private static List<long> ParseInput(string input)
     {
-        return input.Split(",").Select(n=>Convert.ToInt64(n)).ToList();
+        return input.Split(",").Select(n => Convert.ToInt64(n)).ToList();
     }
 
     private static long SimulateFishes(string fishesString, long steps)
@@ -37,7 +35,7 @@ public class Day06 : DayBase
     private static long SimulateFishes(List<long> fishes, long steps)
     {
         var fishesGroups = new long[9];
-        
+
         for (long i = 0; i < 8; i++)
         {
             fishesGroups[i] = fishes.Count(fishAge => fishAge == i);
@@ -46,21 +44,21 @@ public class Day06 : DayBase
         for (long i = 0; i < steps; i++)
         {
             var nextGroup = new long[9];
-            
+
             for (long j = 0; j <= 7; j++)
             {
-                nextGroup[j] = fishesGroups[j+1];
+                nextGroup[j] = fishesGroups[j + 1];
             }
 
             nextGroup[6] += fishesGroups[0];
             nextGroup[8] += fishesGroups[0];
-            
+
             fishesGroups = nextGroup;
         }
 
         return fishesGroups.Sum();
     }
-    
+
     [Test]
     public override void RunPart1OnRealInput()
     {
