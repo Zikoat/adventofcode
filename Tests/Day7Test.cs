7a66a1413f7e0f5e2bcb7acb77581fbf2d675ba1
Sigurd Schoeler <sschoeler99@gmail.com>
2021-12-23 11:13:45 +0100

    Day 7 and Day 8

---
+++ Tests/Day7Test.cs
@@ -0,0 +1,33 @@
+﻿using adventofcode2020;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Day7Test
+    {
+        private const string TestInput = @"light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.";
+
+        [Test]
+        public void Part1()
+        {
+            Assert.AreEqual(4, new Day7(TestInput).Part1()); 
+            Assert.AreEqual(-1, new Day7(null).Part1());
+        }
+
+        [Test]
+        public void Part2()
+        {
+            Assert.AreEqual(-1, new Day7(TestInput).Part2());
+            Assert.AreEqual(-1, new Day7(null).Part2());
+        }
+    }
+}
---
+++ Tests/Day8Test.cs
@@ -0,0 +1,25 @@
+﻿using adventofcode2020;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Day8Test
+    {
+        private const string TestInput = @"";
+
+        [Test]
+        public void Part1()
+        {
+            Assert.AreEqual(-1, new Day8(TestInput).Part1());
+            Assert.AreEqual(-1, new Day8(null).Part1());
+        }
+
+        [Test]
+        public void Part2()
+        {
+            // Assert.AreEqual(-1, new Day8(TestInput).Part2());
+            // Assert.AreEqual(-1, new Day8(null).Part2());
+        }
+    }
+}
