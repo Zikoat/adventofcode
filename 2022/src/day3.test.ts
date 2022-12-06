962d78f36288adecc8c3b60a7e44ee59c2321de9
Sigurd Schoeler <sigurd@finexa.no>
2022-12-06 20:41:15 +0100

    d3p2

--- 2022/src/day3.test.ts
+++ 2022/src/day3.test.ts
@@ -17,8 +17,8 @@ test("part1example", () => {
 
   const commonItem = getCommonItem(firstBackpack);
 
-  expect(commonItem).toBe("p");
-  expect(getItemScore(commonItem)).toBe(16);
+  expect(commonItem).toStrictEqual(["p"]);
+  expect(getItemScore(commonItem[0])).toBe(16);
 
   const itemSum = getScoreSum(exampleinput);
   expect(itemSum).toBe(157);
@@ -28,11 +28,47 @@ test("part1", async () => {
   expect(getScoreSum(await getDayInput(3))).toBe(7826);
 });
 
+test("part2Example", () => {
+  const groupScoreSum = getGroupScoreSum(exampleinput);
+  expect(groupScoreSum).toBe(70);
+});
+
+test("part2", async () => {
+  expect(getGroupScoreSum(await getDayInput(3))).toBe(2577);
+});
+
+function getGroupScoreSum(shit: string) {
+  const parsed = shit.split("\n");
+
+  const groups: string[][] = [];
+  let currentGroup: string[] = [];
+
+  for (let i = 0; i < parsed.length; i++) {
+    const backPack = parsed[i];
+    currentGroup.push(backPack);
+    if (i % 3 === 2) {
+      groups.push([...currentGroup]);
+      currentGroup = [];
+    }
+  }
+  
+  const groupScoreSum = sum(groups.map((group) => getGroupScore(group)));
+  return groupScoreSum;
+}
+
+function getGroupScore(group: string[]) {
+  const firstIntersection = intersection(group[1], group[2]);
+  const secondIntersection = intersection(group[0], firstIntersection);
+  const groupItem = secondIntersection;
+  const groupScore = getItemScore(groupItem[0]);
+  return groupScore;
+}
+
 function getScoreSum(inputString: string) {
   const parsed = inputString.split("\n");
 
   const itemScores = parsed.map((backpackString) =>
-    getItemScore(getCommonItem(backpackString))
+    getItemScore(getCommonItem(backpackString)[0])
   );
   const itemSum = sum(itemScores);
   return itemSum;
@@ -49,13 +85,13 @@ function getCommonItem(backpack: string) {
   return commonItem;
 }
 
-// test("part2Example", () => {expect(exampleinput).toBe(-1);});
-// test("part2", async () => {expect(getDayInput(1));});
-
-function intersection(inA: string, inB: string) {
+function intersection(
+  inA: string | Set<string> | string[],
+  inB: string | Set<string> | string[]
+) {
   let a = new Set(inA);
   let b = new Set(inB);
-  let intersection = [...new Set([...a].filter((x) => b.has(x)))][0];
+  let intersection = [...new Set([...a].filter((x) => b.has(x)))];
   return intersection;
 }
 
