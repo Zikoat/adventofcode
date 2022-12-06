cfdec2efe74bb4483cd12a4eb4025ffb23599016
Sigurd Schoeler <sigurd@finexa.no>
2022-12-06 19:08:01 +0100

    d2p2

--- 2022/src/day2.test.ts
+++ 2022/src/day2.test.ts
@@ -9,21 +9,21 @@ C Z`;
 
 test("part1example", () => {
   expect(getScore(["A", "Y"])).toBe(8);
-  expect(getScoreSum(exampleinput)).toBe(15);
+  expect(getScoreSum(exampleinput, getScore)).toBe(15);
 });
 
 test("part1", async () => {
-  expect(getScoreSum(await getDayInput(2))).toBe(12740);
+  expect(getScoreSum(await getDayInput(2), getScore)).toBe(12740);
 });
 
-function getScoreSum(input: string) {
+function getScoreSum(input: string, scoreFunc: (game: ParsedGame) => number) {
   const scores = input
     .split("\n")
     .map(
       (gameString) =>
         gameString.split(" ") as ["A" | "B" | "C", "X" | "Y" | "Z"]
     )
-    .map((game) => getScore(game));
+    .map((game) => scoreFunc(game));
   return sum(scores);
 }
 
@@ -34,6 +34,21 @@ test("getGameResult", () => {
   expect(result).toBe(1);
   result = getScore(["C", "Z"]);
   expect(result).toBe(6);
+});
+
+test("part2Example", () => {
+  let result = getScore2(["A", "Y"]);
+  expect(result).toBe(4);
+  result = getScore2(["B", "X"]);
+  expect(result).toBe(1);
+  result = getScore2(["C", "Z"]);
+  expect(result).toBe(7);
+
+  expect(getScoreSum(exampleinput, getScore2)).toBe(12);
+});
+
+test("part2", async () => {
+  expect(getScoreSum(await getDayInput(2), getScore2)).toBe(11980);
 });
 
 enum Outcome {
@@ -69,13 +84,17 @@ const outcomeScore: { [key in Outcome]: number } = {
   Win: 6,
 };
 
-const shapeScore = {
-  X: 1,
-  Y: 2,
-  Z: 3,
+const shapeScore: { [key in Hand]: number } = {
+  Rock: 1,
+  Paper: 2,
+  Scissor: 3,
 };
 
 type ParsedGame = ["A" | "B" | "C", "X" | "Y" | "Z"];
+
+function getHandOutcome(opponent: Hand, you: Hand) {
+  return outcome[opponent][you];
+}
 
 function getScore(firstGame: ParsedGame): number {
   const opponent = firstGame[0];
@@ -97,9 +116,36 @@ function getScore(firstGame: ParsedGame): number {
   const outCome = getHandOutcome(opponentHand, yourHand);
   const resultScore = outcomeScore[outCome];
 
-  return resultScore + shapeScore[firstGame[1]];
+  return resultScore + shapeScore[yourHand];
 }
 
-function getHandOutcome(opponent: Hand, you: Hand) {
-  return outcome[opponent][you];
+function getScore2(firstGame: ParsedGame): number {
+  const opponent = firstGame[0];
+
+  const opponentHand: Hand = {
+    A: "Rock" as const,
+    B: "Paper" as const,
+    C: "Scissor" as const,
+  }[opponent];
+
+  const wantedResult: Outcome = {
+    X: Outcome.Loss,
+    Y: Outcome.Draw,
+    Z: Outcome.Win,
+  }[firstGame[1]];
+
+  let wantedYourHand: Hand | null = null;
+
+  for (const shit of Object.entries(outcome[opponentHand]) as [
+    Hand,
+    Outcome
+  ][]) {
+    if (shit[1] === wantedResult) wantedYourHand = shit[0];
+  }
+
+  if (!wantedYourHand) throw Error("shit hit the fan");
+
+  const resultScore = outcomeScore[wantedResult];
+
+  return resultScore + shapeScore[wantedYourHand];
 }
