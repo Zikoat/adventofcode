871c83723946c25b2e64b97e9995fd8be2c32795
Sigurd Schoeler <sigurd@finexa.no>
2022-12-08 08:58:34 +0100

    fix test

--- 2022/src/dayX.test.ts
+++ 2022/src/dayX.test.ts
@@ -6,11 +6,11 @@ const exampleinput = ''
 
 test('part1example', () => {
   _.noop()
-  expect(exampleinput).toBe(-1)
+  expect(exampleinput).toBe('')
 })
 
 test('part1', async () => {
-  expect(await getDayInput(-1)).toBe('fail')
+  expect(await getDayInput(1)).toBeTypeOf('string')
 })
 // test("part2Example", () => {expect(exampleinput).toBe(-1);});
 // test("part2", async () => {expect(getDayInput(1));});
--- 2022/src/god.ts
+++ 2022/src/god.ts
@@ -4,6 +4,6 @@ import { join } from 'path'
 
 export async function getDayInput (day: number): Promise<string> {
   return (
-    await readFile(join(__dirname, 'day', day, 'Input.txt'))
+    await readFile(join(__dirname, `day${day.toString()}Input.txt`))
   ).toString()
 }
