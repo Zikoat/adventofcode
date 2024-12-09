161df1124570e0b0e92c64f552674ff8a7937be2
Sigurd Schoeler <sigurd@finexa.no>
2024-12-09 09:47:48 +0100

    d9

--- 2024/days.test.ts
+++ 2024/days.test.ts
@@ -1,14 +1,11 @@
 import { test } from "bun:test";
 
 test.each([
+  // 9,
   8,
   // 7,
   // 6,
-  5,
-  4,
-  3,
-  2,
-  1,
+  5, 4, 3, 2, 1,
 ])("day%i", async (day) => {
   await import(`./d${day}`);
 });
