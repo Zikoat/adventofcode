66b86f972be07ddd91a8d75fb7f57b14689cb3dd
Zikoat <sschoeler99@gmail.com>
2024-12-18 00:12:35 +0100

    d17 try p2 with paralell

--- 2024/d17-worker.ts
+++ 2024/d17-worker.ts
@@ -1,4 +1,22 @@
-import { ass, asseq, assInt, nonNull } from "./common";
+self.onmessage = (event: MessageEvent) => {
+  try {
+    // console.log("from worker.  event data is ");
+    // console.log(event.data);
+    // console.log("doing calc");
+
+    event.data.program.forEach(assInt);
+    assInt(event.data.core);
+    const shit = findSmallestForCore(event.data.program, event.data.core);
+    // console.log("finished calc, returning");
+    postMessage(shit);
+    // console.log("returned");
+  } catch (err) {
+    console.error(err);
+  }
+  process.exit();
+};
+
+import { ass, assInt, nonNull } from "./common";
 
 function getcombo(
   program: Computer["program"],
@@ -8,7 +26,6 @@ function getcombo(
   pointer: number
 ): number {
   const literal = program[pointer + 1];
-  // ass(literal !== undefined);
 
   let combo;
   if (literal === 1 || literal === 0 || literal === 2 || literal === 3)
@@ -19,6 +36,7 @@ function getcombo(
 
   return combo!;
 }
+
 function run(Ain: number, program: Computer["program"]): boolean {
   let pointer = 0;
   let output = 0;
@@ -40,7 +58,6 @@ function run(Ain: number, program: Computer["program"]): boolean {
       if (A !== 0) {
         const literal = program[pointer + 1];
         pointer = literal!;
-        ass(literal! % 2 === 0);
         // hehe lazy
         pointer -= 2;
       }
@@ -68,34 +85,6 @@ function run(Ain: number, program: Computer["program"]): boolean {
   }
 }
 
-const real = `Register A: 38610541
-Register B: 0
-Register C: 0
-
-Program: 2,4,1,1,7,5,1,5,4,3,5,5,0,3,3,0`;
-
-function parse(input: string): Program {
-  const parsed = input
-    .matchAll(
-      /^Register A: (\d+)\nRegister B: (\d+)\nRegister C: (\d+)\n\nProgram: ((?:\d,?)+)$/g
-    )
-    .toArray()[0];
-
-  ass(parsed);
-
-  const program = nonNull(parsed[4])
-    .split(",")
-    .map((a) => {
-      assInt(a);
-      const num = Number(a);
-      ass(num <= 7);
-      ass(num >= 0);
-      return num as 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
-    });
-
-  return program;
-}
-
 type Program = (0 | 1 | 2 | 3 | 4 | 5 | 6 | 7)[];
 
 type Computer = {
@@ -106,38 +95,9 @@ type Computer = {
 };
 
 function findSmallestForCore(program: Program, core: number) {
-  for (let i = core; i < 10000000; i++) {
-    if (i % (1000000 - core) === 0) console.log(i);
+  for (let i = core; i < 10000000000; i += 8) {
+    // if (i % (10000 - core) === 0) console.log(i, "core", core);
     if (run(i, program)) return i;
   }
   return;
 }
-
-function findSmallest(program: Program): void {
-  for (let core = 0; core <= 8; core++) {
-    console.log(core);
-    const worker = new Worker("./d17-worker.ts");
-
-    worker.postMessage({ program: program, core: core });
-
-    worker.onmessage = (event) => {
-      console.log("finished", event.data);
-    };
-    // const out = findSmallestForCore(program, core);
-    // if (out) return out;
-  }
-  // ass(false);
-}
-
-asseq(
-  findSmallest(
-    parse(`Register A: 2024
-Register B: 0
-Register C: 0
-
-Program: 0,3,5,4,3,0`)
-  )
-  // 117440
-);
-
-asseq(findSmallest(parse(real)));
